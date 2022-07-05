c5fd332359def97f14a96587054d0e336801ca28
Narek Aleksanyan <narek.alexanyan.96@gmai.com>
2022-07-05 20:01:15 +0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,17 +4,15 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './assets/styles/index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
