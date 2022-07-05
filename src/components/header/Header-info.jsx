7b11f14d89b2e60ba2f0460145b4736a395d60e7
Narek Aleksanyan <narek.alexanyan.96@gmai.com>
2022-07-05 22:10:44 +0400

    Working on adidas header

--- src/components/header/Header-info.jsx
+++ src/components/header/Header-info.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import classes from "./header.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTruckFast  } from '@fortawesome/free-solid-svg-icons'
 
 const HeaderInfo = () => {
   return (
-    <div>
-      info
+    <div className={classes.headerInfo}>
+      <div className={classes.headerInfo__content}>
+        <div className={classes.headerInfo__content_item}>
+          30% off * starts now
+        </div>
+        <div className={classes.headerInfo__content_item}>
+          <FontAwesomeIcon icon={faTruckFast} />
+          free standard shipping & returns | join now
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default HeaderInfo;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import HeaderInfo from './Header-info';
 import NavBar from './NavBar';
+import classes from './header.module.scss'
 
 const Header = () => {
   return (
-    <div>
+    <div className={classes.header}>
       <HeaderInfo />
       <NavBar />
     </div>
