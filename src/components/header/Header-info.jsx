b054b67beac2962cef0ae28cd0e2696239ab0079
Narek <narek.alexanyan.96@gmail.com>
2022-07-06 17:16:24 +0400

    Adidas header top

--- src/components/header/Header-info.jsx
+++ src/components/header/Header-info.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import classes from "./header.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTruckFast  } from '@fortawesome/free-solid-svg-icons'
+import { faTruckFast } from "@fortawesome/free-solid-svg-icons";
 
 const HeaderInfo = () => {
   return (
     <div className={classes.headerInfo}>
       <div className={classes.headerInfo__content}>
         <div className={classes.headerInfo__content_item}>
-          30% off * starts now
+          <p>30% off * starts now</p>
         </div>
         <div className={classes.headerInfo__content_item}>
           <FontAwesomeIcon icon={faTruckFast} />
-          free standard shipping & returns | join now
+          <p>free standard shipping & returns | join now</p>
         </div>
       </div>
     </div>
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./header.module.scss";
+import logo from "../../assets/icons/icon-adidas-logo.svg"
 
 const NavBar = () => {
+  const headerTopRoutesList = [
+    {
+      id: 1,
+      title: "help",
+      link: "#",
+    },
+    {
+      id: 2,
+      title: "exchanges & returns",
+      link: "#",
+    },
+    {
+      id: 3,
+      title: "order tracker",
+      link: "#",
+    },
+  ];
+
   return (
-    <div>
-      navbar
+    <div className={classes.header__desktop}>
+      <div className={classes.header__desktop_top}>
+        <div className={classes.header__desktop_top_inner}>
+          {headerTopRoutesList.map((route) => (
+            <Link className={classes.routes} key={route.id} to={route.link}>
+              {route.title}
+            </Link>
+          ))}
+          <div className={classes.routes}>join adiclub</div>
+        </div>
+      </div>
+      <div className={classes.header__desktop_bottom}>
+        <div className={classes.header__desktop_bottom_inner}>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default NavBar;
